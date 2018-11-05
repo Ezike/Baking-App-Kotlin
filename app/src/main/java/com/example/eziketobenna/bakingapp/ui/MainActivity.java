7b39d0a9d6c3c82cc915f1014ce9402355a0d1c7
Ezike Tobenna <tobeydaniels@gmail.com>
2018-11-05 08:35:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/eziketobenna/bakingapp/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.eziketobenna.bakingapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.eziketobenna.bakingapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
