241e1c49f47ff91008196a862cde2b7164d46888
Ezike Tobenna <tobeydaniels@gmail.com>
2018-11-09 11:11:13 +0100

    feat: added google sans font file

---
+++ app/src/main/java/com/example/eziketobenna/bakingapp/ui/RecipeFragment.java
@@ -0,0 +1,30 @@
+package com.example.eziketobenna.bakingapp.ui;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.eziketobenna.bakingapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RecipeFragment extends Fragment {
+
+
+    public RecipeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipe, container, false);
+    }
+
+}
