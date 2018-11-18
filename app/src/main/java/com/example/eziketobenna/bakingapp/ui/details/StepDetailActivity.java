94355b89d4363a1e1cd3832c959f3003ee479dcb
Ezike Tobenna <tobeydaniels@gmail.com>
2018-11-18 16:42:15 +0100

    feat: added stpper layout and set toolbar title in detail activity with databinding

--- app/src/main/java/com/example/eziketobenna/bakingapp/ui/details/StepDetailActivity.java
+++ app/src/main/java/com/example/eziketobenna/bakingapp/ui/details/StepDetailActivity.java
@@ -1,13 +1,19 @@
 package com.example.eziketobenna.bakingapp.ui.details;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.eziketobenna.bakingapp.R;
+import com.example.eziketobenna.bakingapp.data.model.Step;
+import com.example.eziketobenna.bakingapp.databinding.ActivityStepDetailBinding;
+import com.stepstone.stepper.StepperLayout;
+import com.stepstone.stepper.VerificationError;
 
 /**
  * An activity representing a single Step detail screen. This
@@ -15,15 +21,18 @@ import com.example.eziketobenna.bakingapp.R;
  * item details are presented side-by-side with a list of items
  * in a {@link StepListActivity}.
  */
-public class StepDetailActivity extends AppCompatActivity {
+public class StepDetailActivity extends AppCompatActivity implements StepperLayout.StepperListener {
+    //    private StepperAdapter mStepperAdapter;
+    Step step;
+    private StepperLayout mStepperLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_step_detail);
-        Toolbar toolbar = findViewById(R.id.detail_toolbar);
+        ActivityStepDetailBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_step_detail);
+        Toolbar toolbar = binding.detailListToolbar;
         setSupportActionBar(toolbar);
-
+        mStepperLayout = binding.stepperLayout;
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -36,12 +45,14 @@ public class StepDetailActivity extends AppCompatActivity {
         // In this case, the fragment will automatically be re-added
         // to its container so we don't need to manually add it.
 
+        step = getIntent().getParcelableExtra((StepDetailFragment.EXTRA));
+        String shortDesc = step.getShortDescription();
+        setTitle(step.getShortDescription());
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
             Bundle arguments = new Bundle();
-            arguments.putParcelable(StepDetailFragment.EXTRA,
-                    getIntent().getParcelableExtra((StepDetailFragment.EXTRA)));
+            arguments.putParcelable(StepDetailFragment.EXTRA, step);
             StepDetailFragment fragment = new StepDetailFragment();
             fragment.setArguments(arguments);
             getSupportFragmentManager().beginTransaction()
@@ -60,5 +71,25 @@ public class StepDetailActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCompleted(View completeButton) {
+
+    }
+
+    @Override
+    public void onError(VerificationError verificationError) {
+
+    }
+
+    @Override
+    public void onStepSelected(int newStepPosition) {
+
+    }
+
+    @Override
+    public void onReturn() {
+
     }
 }
