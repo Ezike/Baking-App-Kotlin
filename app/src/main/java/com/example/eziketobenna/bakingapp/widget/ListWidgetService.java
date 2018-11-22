cdfd840118761a4ae67ee9e8fce92ce3e143c754
Ezike Tobenna <tobeydaniels@gmail.com>
2018-11-22 04:13:40 +0100

    feat: added widget remote service class and remoteview factory

---
+++ app/src/main/java/com/example/eziketobenna/bakingapp/widget/ListWidgetService.java
@@ -0,0 +1,61 @@
+package com.example.eziketobenna.bakingapp.widget;
+
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+public class ListWidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new ListRemoteViewsFactory();
+    }
+
+    class ListRemoteViewsFactory implements
+            RemoteViewsService.RemoteViewsFactory {
+
+        @Override
+        public void onCreate() {
+
+        }
+
+        @Override
+        public void onDataSetChanged() {
+
+        }
+
+        @Override
+        public void onDestroy() {
+
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public RemoteViews getViewAt(int position) {
+            return null;
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 0;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+    }
+}
