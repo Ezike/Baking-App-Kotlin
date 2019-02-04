3d748e96f76ff7b60f285cd66476e8563e8f4035
Tobenna Ezike <Tobeydaniels@gmail.com>
2019-02-04 15:04:35 +0100

    feat: add Baking Application class
    This class extends Application class and will be used for dagger2

---
+++ app/src/main/java/com/example/eziketobenna/bakingapp/BakingApplication.java
@@ -0,0 +1,10 @@
+package com.example.eziketobenna.bakingapp;
+
+import android.app.Application;
+
+public class BakingApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
