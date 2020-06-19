6fa69f5a94361a68b4eae4763cf86f45416cd9a0
eziketobenna <tobeydaniels@gmail.com>
2020-06-19 21:32:50 +0100

    refactor

--- presentation/src/main/java/com/example/eziketobenna/bakingapp/presentation/event/SingleEvent.kt
+++ presentation/src/main/java/com/example/eziketobenna/bakingapp/presentation/event/SingleEvent.kt
@@ -6,13 +6,20 @@ import java.util.concurrent.atomic.AtomicBoolean
  * Reference:
  * https://medium.com/@mdabrowski89/hi-thanks-for-the-grate-article-7659ed09ddd3
  */
-abstract class SingleEvent<T>(private val argument: T) {
+abstract class SingleEvent<T>(private val content: T) {
 
     private val isConsumed = AtomicBoolean(false)
 
     fun consume(action: (T) -> Unit) {
         if (!isConsumed.getAndSet(true)) {
-            action.invoke(argument)
+            action.invoke(content)
         }
     }
+
+    fun reset() {
+        isConsumed.set(false)
+    }
+
+    val peekContent: T
+        get() = content
 }
