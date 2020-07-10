c551d7a8d1f18e9fac3101bc38ec896feb5f04bc
eziketobenna <tobeydaniels@gmail.com>
2020-07-10 17:30:07 +0100

    complete RecipeViewModelTest.kt - fix onBackPressed in MainActivity.kt

--- common/common-test/src/main/java/com/example/eziketobenna/bakingapp/common_test/FakeNavigationDispatcher.kt
+++ common/common-test/src/main/java/com/example/eziketobenna/bakingapp/common_test/FakeNavigationDispatcher.kt
@@ -1,13 +1,24 @@
 package com.example.eziketobenna.bakingapp.common_test
 
+import android.os.Parcelable
 import com.example.eziketobenna.bakingapp.model.RecipeModel
 import com.example.eziketobenna.bakingapp.model.StepInfoModel
 import com.example.eziketobenna.bakingapp.navigation.NavigationDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
 
-val navigationDispatcher: NavigationDispatcher = object : NavigationDispatcher {
-    override fun openRecipeDetail(model: RecipeModel) {}
+object FakeNavigationDispatcher : NavigationDispatcher {
 
-    override fun openStepDetail(stepInfoModel: StepInfoModel) {}
+    val event: MutableStateFlow<Parcelable?> = MutableStateFlow(null)
 
-    override fun goBack() {}
+    override fun openRecipeDetail(model: RecipeModel) {
+        event.value = model
+    }
+
+    override fun openStepDetail(stepInfoModel: StepInfoModel) {
+        event.value = stepInfoModel
+    }
+
+    override fun goBack() {
+        event.value = null
+    }
 }
--- features/recipes/recipe/src/test/java/com/example/eziketobenna/bakingapp/recipe/presentation/RecipeViewModelTest.kt
+++ features/recipes/recipe/src/test/java/com/example/eziketobenna/bakingapp/recipe/presentation/RecipeViewModelTest.kt
@@ -1,16 +1,21 @@
 package com.example.eziketobenna.bakingapp.recipe.presentation
 
+import android.os.Parcelable
+import com.example.eziketobenna.bakingapp.common_test.FakeNavigationDispatcher
 import com.example.eziketobenna.bakingapp.common_test.FlowRecorder
 import com.example.eziketobenna.bakingapp.common_test.MainCoroutineRule
 import com.example.eziketobenna.bakingapp.common_test.TestPostExecutionThread
-import com.example.eziketobenna.bakingapp.common_test.navigationDispatcher
 import com.example.eziketobenna.bakingapp.common_test.recordWith
 import com.example.eziketobenna.bakingapp.domain.usecase.FetchRecipes
+import com.example.eziketobenna.bakingapp.model.RecipeModel
 import com.example.eziketobenna.bakingapp.model.mapper.IngredientModelMapper
 import com.example.eziketobenna.bakingapp.model.mapper.RecipeModelMapper
 import com.example.eziketobenna.bakingapp.model.mapper.StepModelMapper
+import com.example.eziketobenna.bakingapp.recipe.presentation.data.DummyData
 import com.example.eziketobenna.bakingapp.recipe.presentation.data.DummyViewState
 import com.example.eziketobenna.bakingapp.recipe.presentation.fake.FakeRecipeRepository
+import com.example.eziketobenna.bakingapp.recipe.presentation.fake.FakeRecipeRepository.Companion.ERROR_MSG
+import com.example.eziketobenna.bakingapp.recipe.presentation.fake.ResponseType
 import com.example.eziketobenna.bakingapp.recipe.presentation.mvi.RecipeViewIntent
 import com.example.eziketobenna.bakingapp.recipe.presentation.mvi.RecipeViewState
 import com.example.eziketobenna.bakingapp.recipe.presentation.processor.RecipeActionProcessor
@@ -18,7 +23,6 @@ import com.example.eziketobenna.bakingapp.recipe.presentation.processor.RecipeVi
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.TestCoroutineScope
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -28,41 +32,197 @@ class RecipeViewModelTest {
     val mainCoroutineRule = MainCoroutineRule()
 
     private val stateRecorder: FlowRecorder<RecipeViewState> = FlowRecorder(TestCoroutineScope())
+    private val navigationEventRecorder: FlowRecorder<Parcelable?> =
+        FlowRecorder(TestCoroutineScope())
 
     private val recipeModelMapper = RecipeModelMapper(StepModelMapper(), IngredientModelMapper())
     private val recipeViewStateReducer = RecipeViewStateReducer(recipeModelMapper)
-    private val fetchRecipes = FetchRecipes(FakeRecipeRepository(), TestPostExecutionThread())
+    private val fakeRecipeRepository = FakeRecipeRepository()
+    private val fetchRecipes = FetchRecipes(fakeRecipeRepository, TestPostExecutionThread())
     private val recipeActionProcessor = RecipeActionProcessor(fetchRecipes)
 
     private val dummyViewState = DummyViewState(recipeModelMapper)
 
-    private lateinit var recipeViewModel: RecipeViewModel
-
-    @Before
-    fun setup() {
-        mainCoroutineRule.pauseDispatcher()
-        recipeViewModel = RecipeViewModel(
+    private val recipeViewModel: RecipeViewModel by lazy {
+        RecipeViewModel(
             recipeActionProcessor,
             recipeViewStateReducer,
             RecipeViewIntentProcessor(),
-            navigationDispatcher
+            FakeNavigationDispatcher
         )
     }
 
     @Test
-    fun `loadInitialViewState is emitted when loadInitialAction is processed`() {
+    fun `loadInitialViewState is emitted when loadInitialAction is processed and returns data`() {
+        /**
+         * Pause the dispatcher so that coroutines don't run yet.
+         * This allows us capture the initial viewState emitted from [RecipeViewModel.viewState].
+         * That emission usually gets lost before we subscribe to the stream.
+         */
+        mainCoroutineRule.pauseDispatcher()
         recipeViewModel.viewState.recordWith(stateRecorder)
+        // Resume the dispatcher and then run the coroutines
+        // This will then emit loading and loaded state
         mainCoroutineRule.resumeDispatcher()
         assertThat(stateRecorder.takeAll())
-            .containsExactly(*dummyViewState.loadInitialViewState)
+            .containsExactlyElementsIn(dummyViewState.loadInitialViewState).inOrder()
     }
 
     @Test
-    fun `refreshRecipesViewState is emitted when RefreshRecipesAction is processed`() {
-        mainCoroutineRule.resumeDispatcher()
+    fun `emptyViewState is emitted when loadInitialAction is processed and no data returned`() {
+        fakeRecipeRepository.responseType = ResponseType.EMPTY
+        mainCoroutineRule.pauseDispatcher()
         recipeViewModel.viewState.recordWith(stateRecorder)
-        recipeViewModel.processIntent(flowOf(RecipeViewIntent.RecipeRefreshViewIntent))
+        mainCoroutineRule.resumeDispatcher()
         assertThat(stateRecorder.takeAll())
-            .containsExactly(*dummyViewState.refreshRecipesViewState)
+            .containsExactlyElementsIn(dummyViewState.emptyViewState).inOrder()
+    }
+
+    @Test
+    fun `errorViewState is emitted when loadInitialAction is processed and returns error`() {
+        fakeRecipeRepository.responseType = ResponseType.ERROR
+        mainCoroutineRule.pauseDispatcher()
+        recipeViewModel.viewState.recordWith(stateRecorder)
+        mainCoroutineRule.resumeDispatcher()
+        assertThat(stateRecorder.takeAll())
+            .containsExactlyElementsIn(dummyViewState.noDataErrorViewState).inOrder()
+    }
+
+    @Test
+    fun `refreshRecipesViewState is emitted when RefreshRecipesAction is processed and returns data`() {
+        // Pause and resume dispatcher to get the initial States from LoadInitialAction
+        mainCoroutineRule.pauseDispatcher()
+        recipeViewModel.viewState.recordWith(stateRecorder)
+        mainCoroutineRule.resumeDispatcher()
+        recipeViewModel.processIntent(flowOf(RecipeViewIntent.RecipeRefreshViewIntent))
+        // Assert the entire stream starting from LoadInitialAction
+        assertThat(stateRecorder.takeAll()).containsExactlyElementsIn(
+            dummyViewState.loadInitialViewState +
+                dummyViewState.refreshRecipesViewState
+        ).inOrder()
+    }
+
+    @Test
+    fun `emptyViewState is emitted when RefreshRecipesAction is processed and no data returned`() {
+        mainCoroutineRule.pauseDispatcher()
+        fakeRecipeRepository.responseType = ResponseType.EMPTY
+        recipeViewModel.viewState.recordWith(stateRecorder)
+        mainCoroutineRule.resumeDispatcher()
+        recipeViewModel.processIntent(flowOf(RecipeViewIntent.RecipeRefreshViewIntent))
+        assertThat(stateRecorder.takeAll()).containsExactlyElementsIn(
+            dummyViewState.emptyViewState + arrayOf(
+                dummyViewState.emptyViewState.last().refreshingState,
+                dummyViewState.emptyViewState.last().refreshingState.emptyState
+            )
+        ).inOrder()
+    }
+
+    @Test
+    fun `noDataErrorViewState is emitted when RefreshRecipesAction is processed and no data returned`() {
+        mainCoroutineRule.pauseDispatcher()
+        fakeRecipeRepository.responseType = ResponseType.ERROR
+        recipeViewModel.viewState.recordWith(stateRecorder)
+        mainCoroutineRule.resumeDispatcher()
+        recipeViewModel.processIntent(flowOf(RecipeViewIntent.RecipeRefreshViewIntent))
+        assertThat(stateRecorder.takeAll()).containsExactlyElementsIn(
+            dummyViewState.noDataErrorViewState + arrayOf(
+                dummyViewState.noDataErrorViewState.last().refreshingState,
+                dummyViewState.noDataErrorViewState.last().refreshingState
+                    .noDataErrorState(ERROR_MSG)
+            )
+        ).inOrder()
+    }
+
+    @Test
+    fun `dataAvailableViewState is emitted when RefreshRecipesAction is processed but data is already loaded`() {
+        // First set the fetchRecipes response to data
+        mainCoroutineRule.pauseDispatcher()
+        fakeRecipeRepository.responseType = ResponseType.DATA
+        recipeViewModel.viewState.recordWith(stateRecorder)
+        mainCoroutineRule.resumeDispatcher()
+        // Reset fetchRecipes response to error before sending new intent,
+        // so as to simulate a `data available error state` [Transient error]
+        fakeRecipeRepository.responseType = ResponseType.ERROR
+        recipeViewModel.processIntent(flowOf(RecipeViewIntent.RecipeRefreshViewIntent))
+        assertThat(stateRecorder.takeAll()).containsExactlyElementsIn(
+            dummyViewState.loadInitialViewState + arrayOf(
+                dummyViewState.loadInitialViewState.last().refreshingState,
+                dummyViewState.loadInitialViewState.last().refreshingState
+                    .dataAvailableErrorState(ERROR_MSG)
+            )
+        ).inOrder()
+    }
+
+    @Test
+    fun `retryFetchViewState is emitted when RetryFetchAction is processed and data returned`() {
+        mainCoroutineRule.pauseDispatcher()
+        fakeRecipeRepository.responseType = ResponseType.ERROR
+        recipeViewModel.viewState.recordWith(stateRecorder)
+        mainCoroutineRule.resumeDispatcher()
+        fakeRecipeRepository.responseType = ResponseType.DATA
+        recipeViewModel.processIntent(flowOf(RecipeViewIntent.RetryFetchViewIntent))
+        assertThat(stateRecorder.takeAll())
+            .containsExactlyElementsIn(
+                dummyViewState.noDataErrorViewState +
+                    dummyViewState.retryFetchViewState
+            ).inOrder()
+    }
+
+    @Test
+    fun `emptyViewState is emitted when RetryFetchAction is processed and no data returned`() {
+        mainCoroutineRule.pauseDispatcher()
+        fakeRecipeRepository.responseType = ResponseType.EMPTY
+        recipeViewModel.viewState.recordWith(stateRecorder)
+        mainCoroutineRule.resumeDispatcher()
+        recipeViewModel.processIntent(flowOf(RecipeViewIntent.RetryFetchViewIntent))
+        assertThat(stateRecorder.takeAll())
+            .containsExactlyElementsIn(
+                dummyViewState.emptyViewState + arrayOf(
+                    dummyViewState.emptyViewState.last().loadingState,
+                    dummyViewState.emptyViewState.last().loadingState.emptyState
+                )
+            ).inOrder()
+    }
+
+    @Test
+    fun `noDataErrorViewState is emitted when RetryFetchAction is processed and no data returned`() {
+        mainCoroutineRule.pauseDispatcher()
+        fakeRecipeRepository.responseType = ResponseType.ERROR
+        recipeViewModel.viewState.recordWith(stateRecorder)
+        mainCoroutineRule.resumeDispatcher()
+        recipeViewModel.processIntent(flowOf(RecipeViewIntent.RetryFetchViewIntent))
+        assertThat(stateRecorder.takeAll())
+            .containsExactlyElementsIn(
+                dummyViewState.noDataErrorViewState + arrayOf(
+                    dummyViewState.emptyViewState.last().loadingState,
+                    dummyViewState.emptyViewState.last().loadingState.noDataErrorState(ERROR_MSG)
+                )
+            ).inOrder()
+    }
+
+    @Test
+    fun `dataAvailableErrorViewState is emitted when recipes are loaded but RetryFetchAction errors`() {
+        mainCoroutineRule.pauseDispatcher()
+        fakeRecipeRepository.responseType = ResponseType.DATA
+        recipeViewModel.viewState.recordWith(stateRecorder)
+        mainCoroutineRule.resumeDispatcher()
+        fakeRecipeRepository.responseType = ResponseType.ERROR
+        recipeViewModel.processIntent(flowOf(RecipeViewIntent.RetryFetchViewIntent))
+        assertThat(stateRecorder.takeAll())
+            .containsExactlyElementsIn(
+                dummyViewState.loadInitialViewState +
+                    dummyViewState.dataErrorViewState
+            ).inOrder()
+    }
+
+    @Test
+    fun `openRecipeDetail calls NavigationDispatcher`() {
+        FakeNavigationDispatcher.event.recordWith(navigationEventRecorder)
+        val model: RecipeModel = recipeModelMapper.mapToModel(DummyData.recipe)
+        recipeViewModel.openRecipeDetail(model)
+        val events: List<Parcelable?> = navigationEventRecorder.takeAll().filterNotNull()
+        assertThat(events.size).isEqualTo(1)
+        assertThat(events[0]).isNotNull()
+        assertThat(events[0]).isEqualTo(model)
     }
 }
